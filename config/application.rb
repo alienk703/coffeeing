aad46e6bb40e7577a7d806dccde27c3ee9dc3d28
alienk703 <alienk703@gmail.com>
2014-10-19 20:48:42 -0400

    precompiling false

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,6 @@
+
+config.assets.initialize_on_precompile = false
+
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
@@ -12,6 +15,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 
 module Coffeeing
   class Application < Rails::Application
